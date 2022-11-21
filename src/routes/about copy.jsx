d260aae10315c8fcc9fc2b13cf54421514e25f6f
devJasonClarke <devJasonClarke@users.noreply.github.com>
2022-11-21 03:51:11 +0000

    style: Apply formatting changes

--- src/routes/about copy.jsx
+++ src/routes/about copy.jsx
@@ -3,13 +3,13 @@ import { Link } from "react-router-dom";
 const about = () => {
   return (
     <div className="app">
-        <h1>About</h1>
-                <Link to="/contact">Contact</Link>
-             <div className="btn">
+      <h1>About</h1>
+      <Link to="/contact">Contact</Link>
+      <div className="btn">
         <p>nope</p>
-        </div>
       </div>
+    </div>
   );
 };
 
-export     default      about;
+export default about;
