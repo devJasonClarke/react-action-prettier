09a044513dab5b5f6532a09ba00ade63e689480d
jettison200 <jettison200@gmail.com>
2022-11-20 23:04:11 -0500

    feat: changed prettier rules

---
+++ test folder/test copy 6.jsx
@@ -0,0 +1,9 @@
+const text = ()=>{
+    return (
+                                                                <div className="">
+            <h1>Test        </h1>
+        </div>  
+    )
+}
+
+export default text;
